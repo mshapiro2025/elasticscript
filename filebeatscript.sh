5f6fc37ceb2a4182f599b3deab20ffe517e3ff14
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2022-12-07 12:48:18 -0500

    Update filebeatscript.sh

--- filebeatscript.sh
+++ filebeatscript.sh
@@ -13,6 +13,17 @@ read ipaddress
 echo What is the IP of the Kibana server?
 red ipaddress1
 
+sed -i ;s/enabled: false/enabled: true/I' filebeat.yml
+sed -i 's/enabled: false/enabled: true/I' filebeat.yml
 sed -i "s/hosts: \[\"localhost:9200\"\]/hosts: \[\"https:\/\/${ipaddress}:9200\'\]/I" filebeat.yml
 sed -i 's/#\(protocol: "https"\)/\1/I' filebeat.yml
-sed -i
+sed -i "s|host: \"localhost:5601\"|host: \"${ipaddress1}:5601\"|I" filebeat.yml
+sed -i 's/username: "elastic"/username: "filebeatuser"/I' filebeat.yml
+sed -i 's/password: "changeme"/password: "password"/I' filebeat.yml
+sed -i 's/#\(password:*\)/\1/I' filebeat.yml 
+sed -i '187i\  ssl.veritifaction_mode: none' filebeat.yml
+
+sudo service filebeat enable
+sudo service filebeat start
+
+filebeat setup -e
