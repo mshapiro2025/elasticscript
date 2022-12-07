15919dd6ce94cd2c6cd5a1443cf72524752ef61c
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2022-12-07 12:18:15 -0500

    Update winlogscript.ps1

--- winlogscript.ps1
+++ winlogscript.ps1
@@ -8,7 +8,14 @@ $elasticpass = Read-Host -Prompt "Enter your Elastic Winlogbeat password"
 $elastichost = 'https://' + $elasticip + ':9200'
 $kibanahost = 'http://' + $kibanaip + ':5601'
 
-cd "C:/Program Files/Winlogbeat"
+Invoke-WebRequest -UseBasicParsing https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-8.5.2-iwndows-x86_64.zip -OutFile "winlogbeat-8.5.2-windows-x86_64.zip"
+Start-Sleep -s 300
+Expand-Archive winlogbeat-8.5.2-windows-x86_64.zip "C:/Program files"
+cd "C:/Program Files"
+Rename-Item winlogbeat-8.5.2-windows-x86_64.zip "C:/Program Files"
+cd Winlogbeat
+Powershell.exe -ExecutionPolicy Unrestricted -File ./install-service-winlogbeat.ps1
+Start-Sleep -s 60
 
 # Replace the default Kibana IP address with the user-inputted IP
 (Get-Content winlogbeat.yml) -replace "#host: `"localhost:5601`"", "host:`"$kibanahost`"" | Add-Content winlogbeat1.yml
@@ -23,4 +30,18 @@ Rename-Item winlogbeat1.yml winlogbeat.yml
 Remove-Item winlogbeat.yml
 Rename-Item winlogbeat1.yml winlogbeat.yml
 # Replace the default Elasticsearch IP address with the user-inputted IP
-(Get-Content winlogbeat.yml) -replace "hosts: \[`"localhost:9200`"\]", "hosts: [`"$elastichost`"]"
+(Get-Content winlogbeat.yml) -replace "hosts: \[`"localhost:9200`"\]", "hosts: [`"$elastichost`"]" | Add-Content winlogbeat1.yml
+Remove-Item winlogbeat.yml
+Rename-Item winlogbeat1.yml winlogbeat.yml
+
+Invoke-WebRequest -UseBasicParsing https://download.sysinternals.com/files/Sysmon.zip -OutFile "Sysmon.zip"
+Start-Sleep -s 300
+Expand-Archive Sysmon.zip "C:/Program Files"
+cd "C:/Program Files"
+./sysmon --accepteula
+Start-Sleep -s 60
+
+cd "C:/Program Files/Winlogbeat"
+winlogbeat setup -e
+
+
