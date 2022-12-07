8f897f43f4a3ee8fa11df2c3a979100b3cbe54b0
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2022-12-07 13:15:36 -0500

    Update esscript.sh

--- esscript.sh
+++ esscript.sh
@@ -7,7 +7,7 @@ read $2
 
 apt-get update
 
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpgp --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 sudo apt-get install apt-transport-https
 
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
