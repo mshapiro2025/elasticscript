3b80a6600dbd72cd73d62c5fb3659c0c9bb07887
mshapiro2025 <90416755+mshapiro2025@users.noreply.github.com>
2022-12-07 12:36:09 -0500

    Create esscript.sh

---
+++ esscript.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "What is your node name?"
+read $1
+echo "What is your Elastic server IP address?"
+read $2
+
+apt-get update
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpgp --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+sudo apt-get install apt-transport-https
+
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+
+apt-get update && apt-get install elasticsearch
+
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch
+sudo systemctl start elasticsearch
+
+cd /etc/elasticsearch
+sed -i "s/#node.name: node-1/node.name $1/I" elasticsearch.yml
+sed -i "s/#network.host: 192.168.0.1/network.host $2/I" elasticsearch.yml
+
+cd ~
+/usr/share/elasticsearch/bin/elasticsearch-users useradd admin -p password
+/user/share/elasticsearch/bin/elasticsearch-users roles admin -a superuser
+
+/usr/share/elasticsearch/bin/elasticsearch-users useradd kibanauser -p Ch@mpl@1n22
+/usr/share/elasticsearch/bin/elasticsearch-users roles kibanauser -a superuser
+/usr/share/elasticsearch/bin/elasticsearch-users roles kibanauser -a kibana_system
